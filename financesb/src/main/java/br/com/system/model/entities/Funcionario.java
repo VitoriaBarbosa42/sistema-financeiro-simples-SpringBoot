a6387d4bb1ffeffb25fb18cf4ece0d5b96fb58d6
VitoriaBarbosa42 <vitoria42.barbosa@gmail.com>
2025-12-24 11:43:48 -0300

    Alterações na estrutura de dados do banco, com inclusão da tebela users para autenticação de usuario, inclusão das anotações da documentação e inicio do desenvolvimento para implementação so spring security e JWT

--- financesb/src/main/java/br/com/system/model/entities/Funcionario.java
+++ financesb/src/main/java/br/com/system/model/entities/Funcionario.java
@@ -17,9 +17,6 @@ public class Funcionario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String username;
-
     @Column(name = "nomecompleto", nullable = false)
     private String name;
 
---
+++ financesb/src/main/java/br/com/system/model/entities/User.java
@@ -0,0 +1,67 @@
+package br.com.system.model.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity(name = "users")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String login;
+    private String senha;
+    private UserRole role;
+
+    public User(String login, String senha, UserRole role) {
+        this.login = login;
+        this.senha = senha;
+        this.role = role;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("RELE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
